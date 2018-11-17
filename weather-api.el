1f9caa2154e7151ac7848eeb15339c4a7efd7d64
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-17 11:24:59 +0100

    Change the API from Default to Metric

--- weather-api.el
+++ weather-api.el
@@ -12,8 +12,8 @@
   (let ((url-request-method "POST"))
     (url-retrieve-synchronously
      (if country-code
-         (format "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=%s" city-name country-code (weather--read-key))
-       (format "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" city-name (weather--read-key))))))
+         (format "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=%s&units=Metric" city-name country-code (weather--read-key))
+       (format "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=Metric" city-name (weather--read-key))))))
 
 (defun weather-get (city-name &optional country-code)
   (with-current-buffer (weather--get-content-buffer city-name country-code)
